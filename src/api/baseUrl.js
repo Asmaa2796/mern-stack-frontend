10183568adac9f9916b34a1bce1b09c95e4789dc
codegeek777 <codegeek77@gmail.com>
2025-04-20 22:54:30 +0200

    test

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -2,5 +2,6 @@ const config = {
   baseURL: process.env.REACT_APP_API_BASE_URL,
   photoBaseURL: process.env.REACT_APP_PHOTO_URL,
 };
+console.log("API Base URL:", process.env.REACT_APP_API_BASE_URL);
 
 export default config;
