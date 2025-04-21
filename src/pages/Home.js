fa9f8cc0b78da76ef7007aa38773814b25250e71
codegeek777 <codegeek77@gmail.com>
2025-04-21 10:13:52 +0200

    rwst

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,9 +9,9 @@ const Home = () => {
     return (
         <div className='container py-5'>
             <h1>Here all content</h1>
-            <Link to='/books/add'><span>Add Book</span></Link>
+            <Link to='/books/add'><span className="text-color">Add Book</span></Link>
             <br/>
-            <Link to='/books'><span>Books</span></Link>
+            <Link to='/books'><span className="text-color">Books</span></Link>
         </div>
     );
 }
